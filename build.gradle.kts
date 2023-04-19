12b2d594bcb4316518f719d7ea857c81ddb18342
securitydeep <105039776+sdthatte@users.noreply.github.com>
2023-04-19 21:00:22 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,29 @@
-plugins {
-    application 
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins{
+    application()
 }
-
 repositories {
-    mavenCentral() 
+    mavenCentral()
 }
-
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1") 
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+    implementation("com.google.guava:guava:27.0-jre")
 
-    implementation("com.google.guava:guava:27.0-jre") 
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
 }
+    
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+        implementation("com.google.guava:guava:27.0-jre")
 
-application {
-    mainClass.set("demo.App") 
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
 }
 
 tasks.named<Test>("test") {
